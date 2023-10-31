2abdc577d95b4a19c0af243079a377935108be37
reda fezazi <redafezazi1@gmail.com>
2023-10-31 07:45:27 +0100

    First commmit

---
+++ src/header.js
@@ -0,0 +1,16 @@
+import "./styles/header.css";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>CONTINUE EXPLORING</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero, nihil.
+      </p>
+      <a className="btn" href="#">
+        EXPLORE TOURS
+      </a>
+    </header>
+  );
+};
+export default Header;
